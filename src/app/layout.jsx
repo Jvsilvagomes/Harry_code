638b2d16997ab705dc4215d33012aded8e3f2caf
Jvsilvagomes <joao.silvassenai@gmail.com>
2026-08-10 10:43:06 -0300

    arruma estrutura de pagina e faz layout.jsx

---
+++ src/app/layout.jsx
@@ -0,0 +1,21 @@
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/reactToastify.css';
+import Navbar from './components/Navbar';
+import './globals.css';
+
+export const metadata = {
+    title: 'WizzardDex',
+    descripition: 'Enciclopédia dos Bruxos'
+};
+
+export default function RootLayout({ children }){
+    return (
+        <html lang="pt-BR">
+            <body className="bg-slate-950 text-slate-100 min-h-screen flex flex-col font-sans">
+                    <Navbar />
+                    <main className="flex-1 container mx-auto p-6">{children}</main>
+                    <ToastContainer position="bottom-right" theme="dark"/>
+            </body>
+        </html>
+    );
+}
